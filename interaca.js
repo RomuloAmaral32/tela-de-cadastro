6f8c419aac94c4b3ba2db79e9ef67425ef65b0ea
romulo <romuloamaral32@gmail.com>
2024-06-18 19:52:24 -0300

    resolve problemas

--- interaca.js
+++ interaca.js
@@ -1,100 +1,71 @@
-var zero = document.getElementById("p0");
-var primeiro = document.getElementById("p1");
-var segundo = document.getElementById("p2");
-var terceiro = document.getElementById("p3");
-var quarto = document.getElementById("p4");
-var quinto = document.getElementById("p5");
-var sexto = document.getElementById("p6");
 var campinput = document.getElementsByClassName("entrada");
-let corpo= document.getElementById("areadetrabalho");
-var cabecalhopresente=false;
+let corpo = document.getElementById("areadetrabalho");
+var cabecalhopresente = false;
 
 function registro() {
     var todosPreenchidos = true;
-    
-    if (zero.value === "") {
-        zero.style.border = "2px solid red";
-        todosPreenchidos = false;
-    } else {
-        zero.style.border = "";
-    }
+    var campos = Array.from(campinput);
 
-    if (primeiro.value === "") {
-        primeiro.style.border = "2px solid red";
-        todosPreenchidos = false;
-    } else {
-        primeiro.style.border = "";
-    }
-
-    if (segundo.value === "") {
-        segundo.style.border = "2px solid red";
-        todosPreenchidos = false;
-    } else {
-        segundo.style.border = "";
-    }
-
-    if (terceiro.value === "") {
-        terceiro.style.border = "2px solid red";
-        todosPreenchidos = false;
-    } else {
-        terceiro.style.border = "";
-    }
-
-    if (quarto.value === "") {
-        quarto.style.border = "2px solid red";
-        todosPreenchidos = false;
-    } else {
-        quarto.style.border = "";
-    }
-
-    if (quinto.value === "") {
-        quinto.style.border = "2px solid red";
-        todosPreenchidos = false;
-    } else {
-        quinto.style.border = "";
-    }
-
-    if (sexto.value === "") {
-        sexto.style.border = "2px solid red";
-        todosPreenchidos = false;
-    } else {
-        sexto.style.border = "";
-    }
+    // Verifica se todos os campos estão preenchidos
+    campos.forEach(campo => {
+        if (campo.value === "") {
+            campo.style.border = "2px solid red";
+            todosPreenchidos = false;
+        } else {
+            campo.style.border = "";
+        }
+    });
 
     if (todosPreenchidos) {
-        for (var i = 0; i < campinput.length; i++) {
-            campinput[i].style.border = "";
-            campinput[i].value = "";
-        }
-        zero.focus();
-        let tabela= `
-        <tr>
-            <th>1</th>
-            <th>2</th>
-            <th>3</th>
-            <th>4</th>
-            <th>5</th>
-            <th>6</th>
-            <th>7</th>
-        </tr>`
-        if (!cabecalhopresente) {
-            tabela = `
-            <table>
-            <tr ">
-                <th>Código do Produto</th>
-                <th>Marca do Produto</th>
-                <th>Tipo do Produto</th>
-                <th>Categoria do Produto</th>
-                <th>Preço Unitário do Produto</th>
-                <th>Custo do Produto</th>
-                <th>Observações</th>
-            </tr>` + tabela + `</table>`;
-            cabecalhopresente = true;
-        }
+        // Verifica se o código do produto já existe na tabela
+        let codigoProduto = campos[0].value;
+        let existeProduto = false;
 
-       
-corpo.innerHTML += tabela;
+        let linhas = corpo.querySelectorAll("table tr");
+        linhas.forEach(linha => {
+            let celulas = linha.querySelectorAll("td");
+            if (celulas.length > 0 && celulas[0].textContent === codigoProduto) {
+                existeProduto = true;
+            }
+        });
+
+        if (existeProduto) {
+            alert("Já existe um produto com o mesmo código.");
+        } else {
+            let tabela = `
+            <tr>
+                <td>${campos[0].value}</td>
+                <td>${campos[1].value}</td>
+                <td>${campos[2].value}</td>
+                <td>${campos[3].value}</td>
+                <td>${campos[4].value}</td>
+                <td>${campos[5].value}</td>
+                <td>${campos[6].value}</td>
+            </tr>`;
+
+            if (!cabecalhopresente) {
+                tabela = `
+                <table>
+                    <tr>
+                        <th>Código do Produto</th>
+                        <th>Marca do Produto</th>
+                        <th>Tipo do Produto</th>
+                        <th>Categoria do Produto</th>
+                        <th>Preço Unitário do Produto</th>
+                        <th>Custo do Produto</th>
+                        <th>Observações</th>
+                    </tr>
+                    ${tabela}
+                </table>`;
+                cabecalhopresente = true;
+                corpo.innerHTML = tabela;
+            } else {
+                let tabelaExistente = corpo.querySelector("table");
+                tabelaExistente.innerHTML += tabela;
+            }
+
+            campos.forEach(campo => campo.value = "");
+            campos[0].focus();
+        }
     }
-    
-
 }
